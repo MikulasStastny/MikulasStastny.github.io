47941a936cae95a7b505b35272edc085cf5726d5
Mikuláš Šťastný <xstast38@stud.fit.vutbr.cz>
2023-11-10 11:36:31 +0100

    Added navbar links and contact page

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function toggleNavbar(menu_container) {
     menu_container.classList.toggle("change");
 
     let navbar = document.getElementById("navbar-box");
+    let links = document.getElementById("links")
 
     if(navbar.className == "navbar") {
-        navbar.className = "navbar-expanded"
+        navbar.className = "navbar-expanded";
+        setTimeout(() => { links.style.display = "flex"; }, 100);
     }
     else {
         navbar.className = "navbar";
+        setTimeout(() => { links.style.display = "none"; }, 50);
     }
 }
